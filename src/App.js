bae9172274b28150d4fc8398f784228a967df268
LuosRestil <metaltasteslikechicken@gmail.com>
2020-12-10 21:57:20 -0600

    Added state variables and handleChange declarations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 /**
  * START HERE
  * Good luck!
  */
 function App() {
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  handleNameChange = () => {
+    return undefined;
+  }
+
+  handleDescriptionChange = () => {
+    return undefined;
+  }
+
+  handleDateChange =() => {
+    return undefined;
+  }
+
+  handleStarTimeChange = () => {
+    return undefined;
+  }
+
+  handleEndTimeChange = () => {
+    return undefined;
+  }
+
+  handleSubmit = () => {
+    return undefined;
+  }
+
   return (
     <div className="App">
       <form data-testid='form' className='form-group'>
