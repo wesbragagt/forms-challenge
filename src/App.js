96209ba45740ffe03272acfcec9cb93e9c0762e9
AMA <alexanderabram6440@gmail.com>
2020-12-14 11:03:41 -0600

    added comments for form input fields

--- src/App.js
+++ src/App.js
@@ -12,18 +12,24 @@ function App() {
         <h1>Employee Ticket Form</h1>
         {/* Employee Instructions */}
         <h2>Please fill out the fields below to begin tracking your ticket.</h2>
+
+        {/* Ticket Name input field */}
         <label htmlFor='ticket'>Ticket Name:</label>
         <input data-testid='ticket-input' id='ticket' type='text'/>
 
+        {/* Description input field */}
         <label htmlFor='description'>Description:</label>
         <input data-testid='description-input' id='description'/>
 
+        {/* Date input field */}
         <label htmlFor='Date'>Date:</label>
         <input data-testid='date-input' id='date' type='text'/>
-
+        
+        {/* Start time input field */}
         <label htmlFor='startTime'>Start Time:</label>
         <input data-testid='startTime-input' id='startTime' type='text'/>
 
+        {/* End time input field */}
         <label htmlFor='endTime'>End Time:</label>
         <input data-testid='endTime-input' id='endTime' type='text'/>
 
