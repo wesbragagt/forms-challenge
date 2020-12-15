6ad4131dac12c51300e5d257ae03846cb85efe0e
Brian Smith <52840821+LuosRestil@users.noreply.github.com>
2020-12-15 09:58:30 -0600

    Added jsdoc to utils/convertTimeToMinutes.js

--- src/utils/convertTimeToMinutes.js
+++ src/utils/convertTimeToMinutes.js
@@ -1,3 +1,9 @@
+/**
+ * Converts a timestring in format "HH:MMam/pm" to the number of minutes between midnight and the time parameter value
+ * @param {string} time - format "HH:MMam/pm"
+ * @return {number} - number of minutes from midnight to the timestring value 
+ */
+
 const convertTimeToMinutes = (time) => {
   const hrMin = time.slice(0, time.length - 2).split(":");
   let hr = parseInt(hrMin[0]);
